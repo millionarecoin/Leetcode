c2b98faf8e1a7f0a6ba90ee9fa18d2a58ab955ae
millionarecoin <40475750+millionarecoin@users.noreply.github.com>
2021-03-25 14:22:45 -0400

    Anagram

---
+++ compare_string.py
@@ -0,0 +1,16 @@
+s1 = 'mary'
+s2 = 'army'
+output = ''
+if len(s1) == len(s2):
+ for i in range(len(s1)):
+  for j in range(len(s2)):
+ 
+  #print('s1[i]:  ', s1[i])
+   #print('s2[j]:  ', s2[j])
+   if s1[i] == s2[j]:
+    output = output + s1[i]
+
+
+if output == s1:
+ print('both strings are anagrams')
+  
